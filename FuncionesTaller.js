dae8264286904e5650248517d1aa8ef7dcb6b52d
JuanPabloPerea <36460406+JuanPabloPerea@users.noreply.github.com>
2024-02-29 18:31:08 -0500

    Add files via upload

---
+++ FuncionesTaller.js
@@ -0,0 +1,62 @@
+// funcion para recolectar la informacion de asia
+function consumoAsia(){
+    var endPont = document.getElementById("EndPoint").value;
+    fetch(endPont)
+        .then(response => response.json())
+        .then(data => {
+            var categorias1 = [];
+            var key1 = [];
+            for (let i = 0; i < data.length; i++) {
+                // ir donde se pregunta en que continente se encuentra la region
+                if (data[i].continents == 'Asia') {
+                    key1.push(data[i].region);
+                    categorias1.push(data[i].subregion);    
+                }
+            }
+            var graficaAsia = [
+                {
+                    x: key1,
+                    y: categorias1,
+                    type: 'bar'
+                }
+            ]
+            // Creacion de la grafica
+            var graficaEuropa = [
+                {
+                    x: key2,
+                    y: categorias2,
+                    type: 'bar'
+                }
+            ]
+            Plotly.newPlot('myDiv1', graficaAsia);
+        })
+    .catch(error => console.log(error));
+}
+
+// funcion para recolectar la informacion de europa
+function consumoEuropa(){
+    var endPont = document.getElementById("EndPoint").value;
+    fetch(endPont)
+        .then(response => response.json())
+        .then(data => {
+            var categorias1 = [];
+            var key1 = [];
+            for (let i = 0; i < data.length; i++) {
+                // ir donde se pregunta en que continente se encuentra la region
+                if(data[i].continents == 'Europe') {
+                    key1.push(data[i].region);
+                    categorias1.push(data[i].subregion);
+                }
+            }
+            // Creacion de la grafica
+            var graficaAsia = [
+                {
+                    x: key1,
+                    y: categorias1,
+                    type: 'bar'
+                }
+            ]
+        Plotly.newPlot('myDiv2', graficaEuropa);
+    })
+    .catch(error => console.log(error));
+}
