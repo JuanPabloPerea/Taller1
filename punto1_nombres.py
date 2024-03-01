14c1ce516a9ca63adeb1486d1f0ba0171822eeaf
ttellez200 <160737908+ttellez200@users.noreply.github.com>
2024-02-29 22:53:45 -0500

    Update punto1_nombres.py
    añadí la el crear correo y contraseñas.

--- punto1_nombres.py
+++ punto1_nombres.py
@@ -6,15 +6,19 @@ app = Flask(__name__)
 
 nombres = ["Sebastian", "Tanya", "Juan", "Ana", "Carlos", "Laura", "camilo", "Samanta", "Tomas", "Angie"]
 apellidos = ["Tellez", "Pachon", "Hernández", "Celis", "Navia", "Valero", "Gracia", "Beltral", "Buitrago", "Prieto"]
+dominios = ["@gmail.com", "hotmail.com", "yahoo.com", "outlook.com","@gmail.es", "hotmail.es", "yahoo.es", "outlook.es"] 
 
 @app.route('/nombres_ramdom', methods=['GET'])
-def nombres_ramdom():
-    numero_nombres = 10
-    nombres_aleatorios = []
-    for i in range(numero_nombres):
+def datos_aleatorios(numero):    
+    datos = []
+    for i in range(numero):
         nombre = random.choice(nombres)
         apellido = random.choice(apellidos)
         nombre_completo = f"{nombre} {apellido}"
-        nombres_aleatorios.append(nombre_completo)
-    return jsonify
- 
+        orreo = f"{nombre.lower()}{apellido.lower()}@{random.choice(dominios)}"
+        contrasena = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=8 or k=9))
+        datos.append({"nombre": nombre_completo, "correo": correo, "contrasena": contrasena})
+    return jsonify({"datos_aleatorios": datos})
+
+if __name__ == '__main__':
+    app.run(debug=True)
